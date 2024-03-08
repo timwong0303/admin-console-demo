f9aa1f315f52987fc09667586e35adaa7e091fa0
hien.tran <hien.tran@amela.vn>
2024-03-08 15:44:04 +0700

    fix err setting email

---
+++ precache-manifest.6688cb4e98f25bc0511482771b58e4c3.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b741f4337a97c2f51b636041137c748",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4dd68e44973bf8a174a6",
+    "url": "/static/css/2.0f8214c5.chunk.css"
+  },
+  {
+    "revision": "545146541ea92383432f",
+    "url": "/static/css/main.25ac4586.chunk.css"
+  },
+  {
+    "revision": "4dd68e44973bf8a174a6",
+    "url": "/static/js/2.7545b76f.chunk.js"
+  },
+  {
+    "revision": "af5fb9e76bec14179dbd020c6d38e51a",
+    "url": "/static/js/2.7545b76f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc7c867740ae58a606d1",
+    "url": "/static/js/3.8f5ae7ef.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "/static/js/3.8f5ae7ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "269f05bb1cf52b565393",
+    "url": "/static/js/4.eb1d1d6b.chunk.js"
+  },
+  {
+    "revision": "73aea2f6b249805336f747a1f65ca2b9",
+    "url": "/static/js/4.eb1d1d6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "545146541ea92383432f",
+    "url": "/static/js/main.216962ec.chunk.js"
+  },
+  {
+    "revision": "d3d20b1213741f29ddd886dbbf9d8c3a",
+    "url": "/static/js/main.216962ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "113edcf57e8ae00e5a0d",
+    "url": "/static/js/runtime-main.54daaad6.js"
+  },
+  {
+    "revision": "54559d8e35bf5bd0dcf01e66f2dc46a7",
+    "url": "/static/media/CHANGELOG.54559d8e.md"
+  },
+  {
+    "revision": "1b45b30f177da47879c4968d97e56684",
+    "url": "/static/media/LICENSE.1b45b30f.bin"
+  },
+  {
+    "revision": "80c2b1afc2f6907f73ba4cc7b5408c47",
+    "url": "/static/media/README.80c2b1af.md"
+  },
+  {
+    "revision": "4d2f2f4baa617ba08685b4d9118dc029",
+    "url": "/static/media/bg-pricing.4d2f2f4b.jpeg"
+  },
+  {
+    "revision": "6ee66b2cb8a3d9988e37b83b3b4a590e",
+    "url": "/static/media/card-1.6ee66b2c.jpeg"
+  },
+  {
+    "revision": "ae10829f019b0103525a6cd1102ee213",
+    "url": "/static/media/card-2.ae10829f.jpeg"
+  },
+  {
+    "revision": "3122abf4a3e1067926c08dee7684522d",
+    "url": "/static/media/card-profile1-square.3122abf4.jpg"
+  },
+  {
+    "revision": "5b7c1cad66a2efdbf3604615cc9d7d1c",
+    "url": "/static/media/clint-mckoy.5b7c1cad.jpg"
+  },
+  {
+    "revision": "65f44eb9bcb30f1de98dffacf4ff7409",
+    "url": "/static/media/default_avatar.65f44eb9.jpg"
+  },
+  {
+    "revision": "ebe9884b2152a08a525fcd5054019532",
+    "url": "/static/media/image_placeholder.ebe9884b.jpg"
+  },
+  {
+    "revision": "b10f4f4b4492e11874a0a3d6b626d37d",
+    "url": "/static/media/itchidash.b10f4f4b.svg"
+  },
+  {
+    "revision": "35f7d126c07a40a893f3f6cc8d1c3717",
+    "url": "/static/media/lock.35f7d126.jpeg"
+  },
+  {
+    "revision": "59fc83e26c663ade5dcae6e7d56ca970",
+    "url": "/static/media/login.59fc83e2.jpeg"
+  },
+  {
+    "revision": "a80cd7a77206b15ee13a582ea53b4cab",
+    "url": "/static/media/logo-white.a80cd7a7.svg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "aba54d652e5df3b9f1f8024cbeb6630f",
+    "url": "/static/media/marc.aba54d65.jpg"
+  },
+  {
+    "revision": "1eb7fff2a469da3cc6c3311572d7696d",
+    "url": "/static/media/placeholder.1eb7fff2.jpg"
+  },
+  {
+    "revision": "081bf019322fc100b3b84f34d9bff302",
+    "url": "/static/media/product1.081bf019.jpg"
+  },
+  {
+    "revision": "a0e400118b382fc007111de984da7552",
+    "url": "/static/media/product3.a0e40011.jpg"
+  },
+  {
+    "revision": "49b54380ba092ba68ab160f3dd8d5773",
+    "url": "/static/media/register.49b54380.jpeg"
+  },
+  {
+    "revision": "61c4c50e61d68de31c8951af5470a2c2",
+    "url": "/static/media/sidebar-1.61c4c50e.jpg"
+  },
+  {
+    "revision": "d30c9e30978c9278180dcce2bb66c054",
+    "url": "/static/media/sidebar-2.d30c9e30.jpg"
+  },
+  {
+    "revision": "c963330bcd48670a75ffdd38622b1532",
+    "url": "/static/media/sidebar-3.c963330b.jpg"
+  },
+  {
+    "revision": "f34e943fbffb73c68e761828b1c00980",
+    "url": "/static/media/sidebar-4.f34e943f.jpg"
+  }
+]);
